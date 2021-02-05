9752fda4691da01c3cdf436726bbdeb812727203
yyz <yuanyuzhangyyzz@163.com>
2021-02-05 10:51:08 +0800

    project inint ,set the eslint

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import {Login} from './component/Login/Login.js';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+        <Login></Login>
+    </div>
+  );
+}
+
+export default App;
